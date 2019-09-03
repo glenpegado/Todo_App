7543e0878add5dabc063563c39ad2303189b027f
Glen Pegado <glenpegado@gmail.com>
2019-09-03 14:02:58 -0500

    Made an axios request for delete, added new props in todo folders

--- src/App.js
+++ src/App.js
@@ -46,7 +46,10 @@ class App extends Component {
  
   //Delete Todo
   delTodo = (id) => {
-    this.setState({todos: [...this.state.todos.filter(todo => todo.id !== id)]})
+    axios.delete( 'https://jsonplaceholder.typicode.com/todos/${id}' )
+      .then( res => this.setState( {
+        todos: [ ...this.state.todos.filter( todo => todo.id !== id)]
+      }))
   }
 
 
