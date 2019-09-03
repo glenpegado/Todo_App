a00af407f282b8bc26b52a643a9e2d008a16ce06
Glen Pegado <glenpegado@gmail.com>
2019-09-03 12:38:58 -0500

    Added state for the input form

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
 
 export class AddTodo extends Component {
+    state = {
+        title: ''
+    }
+
+    onChange = (e) => this.setState({[e.targer.name]: e.target.value});
+
+
     render() {
         return (
             <form>
                 <input type="text" name="title" 
                 style={{flex: '10', padding: '5px'}}
                 placeholder="Add Todo ..."
+                value={this.state.title}
+                onChange={this.onChange}
                 />
                 <input type="sumbit" 
                 value="submit"
