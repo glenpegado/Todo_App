b9044cef84288fdfba3cf983e5bc642fa0169189
Glen Pegado <glenpegado@gmail.com>
2019-09-03 13:53:13 -0500

    made an axious request from custom JSON placeholder

--- src/App.js
+++ src/App.js
@@ -5,28 +5,20 @@ import AddTodo from './components/AddTodo'
 import About from './components/pages/About'
 import Header from './components/layout/Header'
 import uuid from 'uuid';
+import axios from 'axios'
 
 
 import './App.css';
 class App extends Component {
   state = {
-    todos: [
-      {
-        id: uuid.v4(),
-        title: 'Take out the trash',
-        completed: false
-      },
-      {
-        id: uuid.v4(),
-        title: 'Dinner with wife',
-        completed: true
-      },
-      {
-        id: uuid.v4(),
-        title: 'Meeting with boss',
-        completed: false
-      }
-    ]
+    todos: []
+  }
+
+  componentDidMount(){
+    axios.get( 'https://jsonplaceholder.typicode.com/todos?_limit=10' )
+      .then( res => this.setState( {
+        todos: res.data
+      } ) )
   }
 
   // Toggle Complete
@@ -41,12 +33,14 @@ class App extends Component {
 
   // Add Todo
   addTodo = ( title ) => {
-    const newTodo = {
-      id: uuid.v4(),
+    axios.post('https://jsonplaceholder.typicode.com/todos', {
       title,
       completed: false
-    }
-    this.setState( {todos: [...this.state.todos, newTodo]} ) 
+    })
+      .then( res => this.setState( {
+            todos: [ ...this.state.todos, res.data ]
+          } )
+)
  
   }
  
